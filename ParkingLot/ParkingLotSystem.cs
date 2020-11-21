36c3424a58b82873eaa498ee9ea33a2e10a3e7a4
Lei ZHU <lzhu19@slb.com>
2020-11-21 18:39:28 +0800

    feat:A_parkingLot_contains_this_car_after_park

--- ParkingLot/ParkingLotSystem.cs
+++ ParkingLot/ParkingLotSystem.cs
@@ -1,11 +1,21 @@
-﻿namespace ParkingLot
+﻿using System.Collections.Generic;
+
+namespace ParkingLot
 {
     using System;
     public class ParkingLotSystem
     {
+        private List<string> parkingLot = new List<string>();
+        public List<string> ParkingLot
+        {
+            get { return parkingLot; }
+            set { parkingLot = value; }
+        }
+
         public string Park(string numberPlate)
         {
-            return DateTime.Now.ToString();
+            ParkingLot.Add(numberPlate);
+             return DateTime.Now.ToString();
         }
     }
 }
--- ParkingLotTest/UnitTest1.cs
+++ ParkingLotTest/UnitTest1.cs
@@ -29,5 +29,17 @@ namespace ParkingLotTest
             Assert.IsType<string>(parkingLotSystem.Park(numberPlate));
             //Assert.Contains(ParkingLotSystem.Parkinglot, numberPlate);
         }
+
+        [Fact]
+        public void A_parkingLot_should_contain_this_car_after_park()
+        {
+            //given
+            var parkingLotSystem = new ParkingLotSystem();
+           //when
+            var numberPlate = "XX2345";
+            parkingLotSystem.Park(numberPlate);
+            //then
+            Assert.Contains<string>(numberPlate, parkingLotSystem.ParkingLot);
+        }
     }
 }
