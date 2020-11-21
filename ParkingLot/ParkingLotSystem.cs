ba5c54a7ad70aad9e8c23e82af5b155087cf0139
Lei ZHU <lzhu19@slb.com>
2020-11-21 17:51:55 +0800

    feat:return_something_when_park_a_car

---
+++ ParkingLot/ParkingLotSystem.cs
@@ -0,0 +1,11 @@
+﻿namespace ParkingLot
+{
+    using System;
+    public class ParkingLotSystem
+    {
+        public string Park(string numberPlate)
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
--- ParkingLotTest/UnitTest1.cs
+++ ParkingLotTest/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace ParkingLotTest
 {
     using ParkingLot;
@@ -6,10 +8,16 @@ namespace ParkingLotTest
     public class UnitTest1
     {
         [Fact]
-        public void FisrtTest()
+        public void Sould_return_something_when_park()
         {
-            var class1 = new Class1();
-            Assert.NotNull(class1);
+            //given
+            var parkingLotSystem = new ParkingLotSystem();
+            //when
+            var numberPlate = "XX2345";
+            //then
+            Assert.NotNull(parkingLotSystem.Park(numberPlate));
+            //Assert.IsType(parkingLotSystem.Park(numberPlate), String);
+            //Assert.Contains(ParkingLotSystem.Parkinglot, numberPlate);
         }
     }
 }
