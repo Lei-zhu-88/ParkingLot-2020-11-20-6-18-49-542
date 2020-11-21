41c86a36b25cd6b5ec5225a81024f49585c877a7
Lei ZHU <lzhu19@slb.com>
2020-11-21 20:11:29 +0800

    feat:Car_can_be_fetched_according_to_parking_ticket

--- ParkingLot/ParkingLotSystem.cs
+++ ParkingLot/ParkingLotSystem.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace ParkingLot
 {
     using System;
+
     public class ParkingLotSystem
     {
         private List<string> parkingLot = new List<string>();
+
         public List<string> ParkingLot
         {
             get { return parkingLot; }
@@ -14,8 +17,29 @@ namespace ParkingLot
 
         public string Park(string numberPlate)
         {
+            Random random = new Random();
             ParkingLot.Add(numberPlate);
-             return DateTime.Now.ToString();
+            string parkingTicket = numberPlate + random.Next(100, 999).ToString();
+            return parkingTicket;
+        }
+
+        public string Fetch(string parkingTicket)
+        {
+            var numberPlate = Decode(parkingTicket);
+
+            if (!ParkingLot.Contains(numberPlate))
+            {
+                Console.WriteLine("No excite this car");
+            }
+
+            ParkingLot.Remove(numberPlate);
+            return numberPlate;
+        }
+
+        public string Decode(string parkingTicket)
+        {
+            var numberPlate = parkingTicket.Substring(0, ParkingLot[0].Length);
+            return numberPlate;
         }
     }
 }
--- ParkingLotTest/UnitTest1.cs
+++ ParkingLotTest/UnitTest1.cs
@@ -27,7 +27,6 @@ namespace ParkingLotTest
             var numberPlate = "XX2345";
             //then
             Assert.IsType<string>(parkingLotSystem.Park(numberPlate));
-            //Assert.Contains(ParkingLotSystem.Parkinglot, numberPlate);
         }
 
         [Fact]
@@ -35,11 +34,26 @@ namespace ParkingLotTest
         {
             //given
             var parkingLotSystem = new ParkingLotSystem();
-           //when
+            //when
             var numberPlate = "XX2345";
             parkingLotSystem.Park(numberPlate);
             //then
             Assert.Contains<string>(numberPlate, parkingLotSystem.ParkingLot);
+        }
+
+        [Fact]
+        public void Car_Should_be_fetched_according_to_parking_ticket()
+        {
+            //given
+            var parkingLotSystem = new ParkingLotSystem();
+            //var customer = new Customer();
+            //Customer_should_fetch_his_car_successfully_after_park
+            //when
+            var numberPlate = "XX2345";
+            var parkingTicket = parkingLotSystem.Park(numberPlate);
+            var actual = parkingLotSystem.Fetch(parkingTicket);
+            //then
+            Assert.Equal(numberPlate, actual);
         }
     }
 }
