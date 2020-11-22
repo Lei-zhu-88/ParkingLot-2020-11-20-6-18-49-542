6f9dfd8581765b145b4da15ce6d89d1a6e76fb0f
Lei ZHU <lzhu19@slb.com>
2020-11-22 14:35:15 +0800

    feat:SmartParkingBoy_Should_park_cars_to_more_space_parkingLot_with_cars

--- ParkingLotTest/ParkingBoyTest.cs
+++ ParkingLotTest/ParkingBoyTest.cs
@@ -218,20 +218,5 @@ namespace ParkingLotSystemTest
             Assert.NotEmpty(parkingTicket);
             Assert.Contains<string>("XX2345", actualParkingLot.ParkingCarsList);
         }
-
-        [Fact]
-        public void SmartParkingBoy_Should_park_cars_to_more_space_parkingLot_when_no_car()
-        {
-            //given
-            var parkingLots = new List<ParkingLot>() { { new ParkingLot(2) }, { new ParkingLot(4) }, { new ParkingLot() } };
-            var parkingBoy = new SmartParkingBoy();
-            //when
-            var numberPlate = "XX2345";
-            var parkingTicket = parkingBoy.Park(numberPlate, parkingLots);
-            var expectParkingLot = parkingLots[2];
-            //then
-            Assert.NotEmpty(parkingTicket);
-            Assert.Contains<string>("XX2345", expectParkingLot.ParkingCarsList);
-        }
     }
 }
--- ParkingLotTest/SmartParkingBoyTest.cs
+++ ParkingLotTest/SmartParkingBoyTest.cs
@@ -22,5 +22,29 @@ namespace ParkingLotTest
             Assert.NotEmpty(parkingTicket);
             Assert.Contains<string>("XX2345", expectParkingLot.ParkingCarsList);
         }
+
+        [Theory]
+        [InlineData(1, 2, 4)]
+        [InlineData(2, 4, 5)]
+        public void SmartParkingBoy_Should_park_cars_to_more_space_parkingLot_with_cars(int parkingCapacity1, int parkingCapacity2, int parkingCapacity3)
+        {
+            //given
+            var parkingLots = new List<ParkingLot>() { { new ParkingLot(parkingCapacity1) }, { new ParkingLot(parkingCapacity2) }, { new ParkingLot(parkingCapacity3) } };
+            var smartParkingBoy = new SmartParkingBoy();
+            var parkingBoy = new ParkingBoy();
+            //when
+            for (int i = 0; i < 2; i++)
+            {
+                parkingBoy.Park(i.ToString(), parkingLots[1]);
+                parkingBoy.Park(i.ToString(), parkingLots[2]);
+            }
+
+            var numberPlate = "XX2345";
+            var parkingTicket = smartParkingBoy.Park(numberPlate, parkingLots);
+            var expectParkingLot = parkingLots[2];
+            //then
+            Assert.NotEmpty(parkingTicket);
+            Assert.Contains<string>("XX2345", expectParkingLot.ParkingCarsList);
+        }
     }
 }
